60c8b7f7ab1d30bd81189788c5e08e4642e823f6
Ahmed Kennawi <ahmed.kennawi@kendimed.com>
2023-06-12 07:07:33 +0300

    first commit

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar: React.FC<ISidebarProps> = ({ children, openSidebar }) => {
         id="default-sidebar"
         className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
           openSidebar ? "-translate-x-full " : "-translate-x-0 "
-        } -translate-x-full sm:translate-x-0`}
+        } sm:translate-x-0`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
